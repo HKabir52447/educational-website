ae36b0f1342168f2807216a8ab5b4f844d4b7f2d
MD. Humayun Kabir <humayunkabir52447@gmail.com>
2021-10-04 13:08:07 +0600

    About section added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import video from "../../videos/production ID-5198159.mp4";
+import "./About.css";
+import aboutImg from '../../images/brighnt.jpg'
 
 const About = () => {
-    return (
+  return (
+    <div className="about">
+      <div className="cls-intro">
+        <video width="100%" height="500px" autoPlay muted loop>
+          <source src={video} type="video/mp4"></source>
+        </video>
+      </div>
+      <div className='about-details'>
         <div>
-            <h2>This is About section</h2>
+          <h5 className="text-center pt-4 sub-heading">About Us</h5>
+          <h3 className="text-center heading"> Bright Future </h3>
+          <p>Bright future is academic coaching cetner. It is established in Oct 04,2021. We provided u best teaching services. Where you can compelte your academic syllebous. Science, Arts and Commerce all groups of sub are covered by qualitiful teachers. All teachers are from public university.
+
+          </p>
         </div>
-    );
+        <div>
+    <img src={aboutImg} className='img-fluid ' alt='aboutImg'/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
